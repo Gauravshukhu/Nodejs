c04aee25f25a0ef7616fa14d7c83ebfea057d7d6
Gaurav Shukla <100159334+Gauravshukhu@users.noreply.github.com>
2024-02-26 21:32:36 +0530

    Update repl.js

--- repl.js
+++ repl.js
@@ -3,4 +3,4 @@
 //2:use Variables
 //3:Multiline code -> use loops here
 //4: use (_) to get the latest result
-//5: we can use editior mode -> .editor , ctrl+d use for enter
+//5: we can use editior mode -> .editor , ctrl+d use for enter 
